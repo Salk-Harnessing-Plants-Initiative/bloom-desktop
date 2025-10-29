dc94eabc8b1b3e7aa6e0b408c321311a9544fc33
Elizabeth Berrigan <berri104@gmail.com>
2025-10-28 18:57:56 -0700

    Fix ESLint error: Use require for CopyWebpackPlugin import

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,8 +1,9 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
